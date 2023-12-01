b0e85d0c3eba8d62ad69e7ea2b22be3ea11264d9
Humberto-Matondo <humbertomat99@gmail.com>
2023-12-01 12:26:40 +0100

    The Produto class was created

--- loja/settings.py
+++ loja/settings.py
@@ -9,9 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-from django.contrib.messages import constants
-from pathlib import Path
 import os
+from pathlib import Path
+
+from django.contrib.messages import constants
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,12 +33,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'produto',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 
     #TODO: remover debug toolbar
     'debug_toolbar',
