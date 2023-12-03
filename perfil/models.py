3b8c7e504128486704342a72a9a54f9cb1ea81be
Humberto-Matondo <humbertomat99@gmail.com>
2023-12-03 17:18:16 +0100

    The Pedido  class was created

--- perfil/models.py
+++ perfil/models.py
@@ -1,3 +1,59 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Perfil(models.Model):
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE)
+    data_nascimento = models.DateField()
+    cpf = models.CharField(max_length=11)
+    endereco = models.CharField(max_length=50)
+    numero = models.CharField(max_length=5)
+    complemento = models.CharField(max_length=30)
+    bairro = models.CharField(max_length=30)
+    cep = models.CharField(max_length=30)
+    bairro = models.CharField(max_length=30)
+    estado = models.CharField(
+        max_length=2,
+        default= 'SP',
+        choices=(
+            ('AC', 'Acre'),
+            ('AL', 'Alagoas'),
+            ('AP', 'Amapá'),
+            ('AM', 'Amazonas'),
+            ('BA', 'Bahia'),
+            ('CE', 'Ceará'),
+            ('DF', 'Distrito Federal'),
+            ('ES', 'Espírito Santo'),
+            ('GO', 'Goiás'),
+            ('MA', 'Maranhão'),
+            ('MT', 'Mato Grosso'),
+            ('MS', 'Mato Grosso do Sul'),
+            ('MG', 'Minas Gerais'),
+            ('PA', 'Pará'),
+            ('PB', 'Paraíba'),
+            ('PR', 'Paraná'),
+            ('PE', 'Pernambuco'),
+            ('PI', 'Piauí'),
+            ('RJ', 'Rio de Janeiro'),
+            ('RN', 'Rio Grande do Norte'),
+            ('RS', 'Rio Grande do Sul'),
+            ('RO', 'Rondônia'),
+            ('RR', 'Roraima'),
+            ('SC', 'Santa Catarina'),
+            ('SP', 'São Paulo'),
+            ('SE', 'Sergipe'),
+            ('TO', 'Tocantins'),
+        )
+
+    )
+    
+    def __str__(self):
+        return f'{self.usuario.first_name} {self.usuario.last_name}'
+    
+    def clean(self):
+        pass
+    
+    class Meta:
+        verbose_name = 'Perfil'
+        verbose_name_plural = 'Perfis'
