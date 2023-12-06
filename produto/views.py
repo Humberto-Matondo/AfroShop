54c75a4c48dcfc593b30180992cddf8617d27e35
Humberto-Matondo <humbertomat99@gmail.com>
2023-12-06 21:05:22 +0100

    I added the product details page and organized it

--- produto/views.py
+++ produto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views import View
 from . import models
 # Create your views here.
@@ -9,8 +10,12 @@ class ListaProdutos(ListView):
     template_name = 'produto/lista.html'
     context_object_name = 'produtos'
     paginate_by = 10
-class DetalheProduto(View):
-    pass
+
+class DetalheProduto(DetailView):
+    model = models.Produto
+    template_name = 'produto/detalhe.html'
+    context_object_name = 'produto'
+    slug_url_kwarg = 'slug'
 
 class AdicionarAoCarrinho(View):
     pass
