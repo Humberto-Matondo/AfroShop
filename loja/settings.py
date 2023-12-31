b63ccb8770b8ba9d7fc9692b1e527e4fc240011b
Humberto-Matondo <humbertomat99@gmail.com>
2023-12-31 09:53:36 +0100

    Adicionei Icon e Logotipo

--- loja/settings.py
+++ loja/settings.py
@@ -136,11 +136,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
 ]
 
-#MEDIA_URL = '/media/produto-imagens/2023/12/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = 'media/' 
-MEDIA_ROOT = BASE_DIR / 'media'
 
 MESSAGE_TAGS = {
     constants.DEBUG: 'alert-info',
